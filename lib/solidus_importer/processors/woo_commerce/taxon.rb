2f022189e896090045939b886da00402454bfc05
Yeboster <yeboster@gmail.com>
2022-01-08 21:56:07 +0100

    Fix woocommerce taxon processor

--- lib/solidus_importer/processors/woo_commerce/taxon.rb
+++ lib/solidus_importer/processors/woo_commerce/taxon.rb
@@ -10,7 +10,7 @@ module SolidusImporter
           @data = context.fetch(:data)
 
           self.product = context.fetch(:product)
-          self.taxonomy = @options[:root_taxonomy]
+          self.taxonomy = options[:root_taxonomy]
 
           process_taxons
         end
@@ -28,7 +28,7 @@ module SolidusImporter
           return unless taxon_hierarchy
 
           taxon_hierarchy.split('|').map do |taxon|
-            last_taxon = taxonomy
+            last_taxon = taxonomy.root
             taxon.split('>').each do |taxon|
               last_taxon = last_taxon.children.find_or_create_by(name: taxon, taxonomy_id: taxonomy.id)
             end
