546ad05bf1522df2d4e37fc6873ce79c7a925e87
Yeboster <yeboster@gmail.com>
2022-02-09 20:48:09 +0000

    Add brand taxonomy and extract it from product name

--- lib/solidus_importer/processors/woo_commerce/taxon.rb
+++ lib/solidus_importer/processors/woo_commerce/taxon.rb
@@ -4,13 +4,14 @@ module SolidusImporter
   module Processors
     module WooCommerce
       class Taxon < ::SolidusImporter::Processors::Base
-        attr_accessor :product, :taxonomy
+        attr_accessor :product, :taxonomy, :brand_taxonomy
 
         def call(context)
           @data = context.fetch(:data)
 
           self.product = context.fetch(:product)
           self.taxonomy = options[:root_taxonomy]
+          self.brand_taxonomy = options[:brand_taxonomy]
 
           process_taxons
         end
@@ -20,33 +21,38 @@ module SolidusImporter
         def options
           @options ||= {
             root_taxonomy: Spree::Taxonomy.find_or_create_by(name: 'Categories'),
-            tags_taxonomy: Spree::Taxonomy.find_or_create_by(name: 'Tags')
+            tags_taxonomy: Spree::Taxonomy.find_or_create_by(name: 'Tags'),
+            brand_taxonomy: Spree::Taxonomy.find_or_create_by(name: 'Brand')
           }
         end
 
         def process_taxons
           return unless taxon_hierarchy
 
+          if product_brand
+            add_taxon_to_prod brand_taxonomy.root.children.find_or_create_by(name: product_brand,
+              taxonomy_id: brand_taxonomy.id)
+          end
+
           taxon_hierarchy.split('|').map do |taxon|
             last_taxon = taxonomy.root
-            taxon.split('>').each do |taxon|
-              last_taxon = last_taxon.children.find_or_create_by(name: taxon, taxonomy_id: taxonomy.id)
+            taxon.split('>').each do |sub_taxon|
+              last_taxon = last_taxon.children.find_or_create_by(name: sub_taxon, taxonomy_id: taxonomy.id)
             end
 
-            add_taxon(last_taxon)
+            add_taxon_to_prod(last_taxon)
           end
         end
 
-        def add_taxon(taxon)
+        def add_taxon_to_prod(taxon)
           product.taxons << taxon unless product.taxons.include?(taxon)
         end
 
+        def product_brand
+          splitted = @data['Product Name']&.split(' – ')
+          return unless splitted&.size&.> 1
 
-        def prepare_taxon(name, parent_taxon)
-          Spree::Taxon.find_or_initialize_by(
-            name: name,
-            taxonomy_id: taxonomy.id
-          )
+          splitted.first.strip.capitalize
         end
 
         def taxon_hierarchy
